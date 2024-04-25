38960ddb0fc07ebf5b6ddb54108955b8b9fa05ea
jonasfabian <j.fabian@gmx.ch>
2024-04-25 15:14:27 +0200

    add better styling for projects and websites

--- script.js
+++ script.js
@@ -28,6 +28,45 @@ const createDomElement = (tag, content, cssClass) => {
     return domElement;
 }
 
+/*
+* Checks the clients timezone and return a time of day string accordingly
+* */
+const parseTimeZone = () => {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const currentTime = new Date();
+    const hours = currentTime.toLocaleTimeString('en-US', {timeZone: userTimeZone, hour: '2-digit', hour12: false});
+    if (hours < 11) return 'morning';
+    if (hours < 13) return 'day';
+    if (hours < 17) return 'afternoon';
+    if (hours < 21) return 'evening';
+    if (hours < 24) return 'night';
+}
+
+const renderAbout = () => {
+    const aboutContainer = createDomElement('div', [], 'aboutContainer');
+
+    // Avatar, Name and Position Container
+    const nameAndAvatarContainer = createDomElement('div', [], 'nameAndAvatarContainer');
+
+    const avatar = createDomElement('img', ['../assets/profile-image.jpg'], 'avatar');
+
+    const nameContainer = createDomElement('div', [], 'nameContainer');
+    const name = createDomElement('h1', 'Jonas Fabian', 'name');
+    const position = createDomElement('div', 'Product-focused Software Engineer');
+
+    nameContainer.append(name, position);
+    nameAndAvatarContainer.append(avatar, nameContainer);
+
+    // About Text Container
+    const aboutTextContainer = createDomElement('div', [], 'aboutTextContainer');
+    const aboutTextTitle = createDomElement('div', 'About me', 'aboutTextHeader');
+    const aboutText = createDomElement('div', `Good ${parseTimeZone()}, I'm Jonas. I'm passionate about creating innovative products from conception to completion. I specialize in designing user-friendly experiences that evolve and enhance over time through data-driven insights. Committed to consistent delivery.`, 'aboutText');
+    aboutTextContainer.append(aboutTextTitle, aboutText);
+
+    aboutContainer.append(nameAndAvatarContainer, aboutTextContainer);
+
+    return aboutContainer;
+}
 
 const renderProjects = (projects) => {
     const projectContainers = createDomElement('div', [], 'projectContainers');
@@ -74,54 +113,66 @@ const renderProjects = (projects) => {
     return projectContainers;
 }
 
-const renderAbout = () => {
-    const aboutContainer = createDomElement('div', [], 'aboutContainer');
+const renderWebsites = (websites) => {
+    const websiteContainers = createDomElement('div', [], 'websiteContainers');
+    const websiteContainersTitle = createDomElement('div', 'Other Projects', 'websiteContainersTitle');
 
-    // Avatar, Name and Position Container
-    const nameAndAvatarContainer = createDomElement('div', [], 'nameAndAvatarContainer');
+    websiteContainers.append(websiteContainersTitle);
 
-    const avatar = createDomElement('img', ['../assets/profile-image.jpg'], 'avatar');
+    websites.forEach(website => {
+        const websiteContainer = createDomElement('div', [], 'websiteContainer');
 
-    const nameContainer = createDomElement('div', [], 'nameContainer');
-    const name = createDomElement('h1', 'Jonas Fabian', 'name');
-    const position = createDomElement('div', 'Product-focused Software Engineer');
+        const arrowLink = createDomElement('img', '../assets/arrow-link.svg', 'arrowLink');
+        const titleSpan = createDomElement('span', website.name);
+        const websiteName = createDomElement('a', [website.url, titleSpan], 'websiteTitleContainer');
+        websiteName.appendChild(arrowLink);
 
-    nameContainer.append(name, position);
-    nameAndAvatarContainer.append(avatar, nameContainer);
+        // Other project details
+        const websiteStartDate = createDomElement('span', website.startDate);
+        const websiteEndDate = createDomElement('span', website.endDate !== '' ? " - " + website.endDate : '');
+        const websiteDuration = createDomElement('div', [], 'websiteDuration');
+        websiteDuration.append(websiteStartDate);
+        websiteName.append(websiteEndDate);
 
-    // About Text Container
-    const aboutTextContainer = createDomElement('div', [], 'aboutTextContainer');
-    const aboutTextTitle = createDomElement('div', 'About', 'aboutTextHeader');
-    const aboutText = createDomElement('div', `Good ${parseTimeZone()}, I'm Jonas. I'm passionate about creating innovative products from conception to completion. I specialize in designing user-friendly experiences that evolve and enhance over time through data-driven insights. Committed to consistent delivery.`, 'aboutText');
-    aboutTextContainer.append(aboutTextTitle, aboutText);
+        const websiteDescription = createDomElement('span', website.description, 'websiteDescription');
+        const websiteLogo = createDomElement('img', '../assets/logos/' + website.logo, 'websiteLogo');
+        const websiteTechnologies = createDomElement('div', [], 'technologies');
 
-    aboutContainer.append(nameAndAvatarContainer, aboutTextContainer);
+        // Append technologies of project to projectTechnologies element
+        website.technologies.forEach((technology, index) => {
+            if (index !== 0 && index !== website.technologies.length)
+                websiteTechnologies.appendChild(createDomElement('span', '•', 'technology'));
+            websiteTechnologies.append(createDomElement('span', technology.name, 'technology'))
+        });
 
-    return aboutContainer;
-}
+        websiteContainer.append(
+            websiteName,  // This now includes both the title and the link
+            websiteDuration,
+            websiteDescription,
+            // projectLogo,
+            websiteTechnologies
+        );
 
-/*
-* Checks the clients timezone and return a time of day string accordingly
-* */
-const parseTimeZone = () => {
-    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const currentTime = new Date();
-    const hours = currentTime.toLocaleTimeString('en-US', {timeZone: userTimeZone, hour: '2-digit', hour12: false});
-    if (hours < 11) return 'morning';
-    if (hours < 13) return 'day';
-    if (hours < 17) return 'afternoon';
-    if (hours < 21) return 'evening';
-    if (hours < 24) return 'night';
+        websiteContainers.appendChild(websiteContainer);
+    });
+    return websiteContainers;
 }
 
 const initializeContent = (jsonName, htmlName) => {
     loadHtmlFile(htmlName)
-        .then(html => main.innerHTML = html)
-        .then(() => loadJSON(jsonName))
-        .then(projects => {
-            main.appendChild(renderAbout())
+        .then(html => {
+            main.innerHTML = html;
+            return Promise.all([loadJSON(jsonName), loadJSON('websites')]);
+        })
+        .then(([projects, websites]) => {
             projects = projects.sort((p1, p2) => p1.endDate < p2.endDate ? 1 : -1);
-            main.appendChild(renderProjects(projects))
+            websites = websites.sort((p1, p2) => p1.startDate < p2.startDate ? 1 : -1);
+            main.appendChild(renderAbout());
+            main.appendChild(renderProjects(projects));
+            main.appendChild(renderWebsites(websites));
+        })
+        .catch(error => {
+            console.error('Failed to load data:', error); // Error handling
         });
 }
 
