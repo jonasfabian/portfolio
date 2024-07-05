9ee9c8d98096e971c5da278ff222390cbe5688c3
jonasfabian <j.fabian@gmx.ch>
2024-07-05 15:03:12 +0200

    add collision check

--- snake.js
+++ snake.js
@@ -1,6 +1,3 @@
-let canvas;
-let ctx;
-
 Number.prototype.mod = function (n) {
     "use strict";
     return ((this % n) + n) % n;
@@ -11,8 +8,12 @@ const CELL_SIZE = 20;
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 300;
 
+let canvas;
+let ctx;
 let player;
 let directionState;
+
+let itemList = [];
 
 /**
  * Player constructor
@@ -75,6 +76,40 @@ const initPlayer = () => {
 }
 
 /**
+ * Adds an item to the items-list
+ * */
+const addItem = () => {
+    itemList.push(
+        {
+            color: "",
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+            position: {
+                x: 20,
+                y: 40
+            }
+        }
+    );
+}
+
+/**
+ * Draws all items in itemList
+ * */
+const drawItems = (ctx) => {
+    itemList.forEach(item =>
+        drawRectangle(item.position.x, item.position.y, item.width, item.height, "yellow", ctx));
+}
+
+/**
+ * Redraws the game-grid and player
+ * */
+const redraw = () => {
+    drawGameGrid(10, ctx);
+    drawPlayer(ctx);
+    drawItems(ctx);
+}
+
+/**
  * Initializes the canvas
  * */
 const initCanvas = () => {
@@ -82,34 +117,57 @@ const initCanvas = () => {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
     ctx = canvas.getContext("2d");
+    itemList = [];
     drawGameGrid(10, ctx);
     initPlayer();
     drawPlayer(ctx);
+    addItem();
+    drawItems(ctx);
 }
 initCanvas();
 
-const redraw = () => {
-    drawGameGrid(10, ctx);
-    drawPlayer(ctx);
+/**
+ * Checks whether two items have a collision
+ * */
+const checkCollision = (item1, item2) => {
+    if (
+        item1.position.x < item2.position.x + item2.width &&
+        item1.position.x + item1.width > item2.position.x &&
+        item1.position.y < item2.position.y + item2.height &&
+        item1.position.y + item1.height > item2.position.y
+    ) {
+        return true;
+    }
+    return false;
 }
 
+/**
+ * Checks whether the player has collision with an item
+ * */
+const playerHasCollisionWithItem = () => {
+    return itemList.some(item => checkCollision(player, item));
+}
 
 setInterval(() => {
     switch (directionState) {
         case 'UP' :
             player.position.y = (player.position.y -= CELL_SIZE).mod(CANVAS_HEIGHT);
+            if (playerHasCollisionWithItem()) alert("Collision");
             redraw();
             break;
         case 'LEFT' :
             player.position.x = (player.position.x -= CELL_SIZE).mod(CANVAS_WIDTH);
+            if (playerHasCollisionWithItem()) alert("Collision");
             redraw();
             break;
         case 'DOWN' :
             player.position.y = (player.position.y += CELL_SIZE).mod(CANVAS_HEIGHT);
+            if (playerHasCollisionWithItem()) alert("Collision");
             redraw();
             break;
         case 'RIGHT' :
             player.position.x = (player.position.x += CELL_SIZE).mod(CANVAS_WIDTH);
+            if (playerHasCollisionWithItem()) alert("Collision");
             redraw();
             break;
         default :
@@ -118,7 +176,7 @@ setInterval(() => {
     }
 }, REFRESH_RATE);
 
-document.addEventListener('keydown', function(e){
+document.addEventListener('keydown', function (e) {
     switch (e.key) {
         case 'w' :
             directionState = 'UP';
