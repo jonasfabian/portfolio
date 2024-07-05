974bb7dafdeb18886d5ca5729ecd0bb4c0ce9a6b
jonasfabian <j.fabian@gmx.ch>
2024-07-05 14:31:59 +0200

    add more logic

--- snake.js
+++ snake.js
@@ -1,43 +1,137 @@
 let canvas;
 let ctx;
 
+Number.prototype.mod = function (n) {
+    "use strict";
+    return ((this % n) + n) % n;
+};
+
+const REFRESH_RATE = 1000;
+const CELL_SIZE = 20;
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 300;
 
-const drawLine = (from, to, ctx) => {
-    ctx.moveTo(from.x, from.y);
-    ctx.lineTo(to.x, to.y);
-    ctx.stroke();
+let player;
+let directionState;
+
+/**
+ * Player constructor
+ * */
+const Player = (width, height, position, color) => {
+    return {
+        width: width,
+        height: height,
+        position: position,
+        color: color
+    }
 }
 
-const drawGrid = (numberOfCells, ctx) => {
-    const cellWidth = CANVAS_WIDTH / numberOfCells;
-    let x = cellWidth;
-    let y = cellWidth;
+/**
+ * Draws a rectangle with coordinates, width, height, and canvas-context provided
+ * */
+const drawRectangle = (x, y, width, height, color, ctx) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+/**
+ * Draws a player
+ * */
+const drawPlayer = (ctx) => {
+    drawRectangle(player.position.x, player.position.y, player.width, player.height, player.color, ctx);
+}
+
+/**
+ * Draws the game grid
+ * */
+const drawGameGrid = (numberOfCells, ctx) => {
+    let x = 0;
+    let y = 0;
+
     while (x < CANVAS_WIDTH) {
-        // draw vertical line
-        const from = {x: x, y: 0};
-        const to = {x: x, y: CANVAS_HEIGHT};
-        drawLine(from, to, ctx);
-        // increment x by cellWidth
-        x += cellWidth;
-    }
-    while (y < CANVAS_HEIGHT) {
-        // draw horizontal lines
-        const from = {x: 0, y: y}
-        const to = {x: CANVAS_WIDTH, y: y}
-        drawLine(from, to, ctx)
-        // increment y by cellWidth
-        y += cellWidth;
+        y = 0;
+        while (y < CANVAS_HEIGHT) {
+            drawRectangle(x, y, CELL_SIZE, CELL_SIZE, "blue", ctx);
+            y += CELL_SIZE;
+        }
+        x += CELL_SIZE;
     }
 }
 
+/**
+ * Initializes the player
+ * */
+const initPlayer = () => {
+    const playerPosition = {
+        x: CELL_SIZE,
+        y: CELL_SIZE
+    }
+    player = Player(
+        CELL_SIZE,
+        CELL_SIZE,
+        playerPosition,
+        "purple"
+    )
+}
+
+/**
+ * Initializes the canvas
+ * */
 const initCanvas = () => {
     canvas = document.getElementById("canvas");
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
     ctx = canvas.getContext("2d");
-    drawGrid(10, ctx);
+    drawGameGrid(10, ctx);
+    initPlayer();
+    drawPlayer(ctx);
 }
 initCanvas();
+
+const redraw = () => {
+    drawGameGrid(10, ctx);
+    drawPlayer(ctx);
+}
+
+
+setInterval(() => {
+    switch (directionState) {
+        case 'UP' :
+            player.position.y = (player.position.y -= CELL_SIZE).mod(CANVAS_HEIGHT);
+            redraw();
+            break;
+        case 'LEFT' :
+            player.position.x = (player.position.x -= CELL_SIZE).mod(CANVAS_WIDTH);
+            redraw();
+            break;
+        case 'DOWN' :
+            player.position.y = (player.position.y += CELL_SIZE).mod(CANVAS_HEIGHT);
+            redraw();
+            break;
+        case 'RIGHT' :
+            player.position.x = (player.position.x += CELL_SIZE).mod(CANVAS_WIDTH);
+            redraw();
+            break;
+        default :
+            redraw();
+            break;
+    }
+}, REFRESH_RATE);
+
+document.addEventListener('keydown', function(e){
+    switch (e.key) {
+        case 'w' :
+            directionState = 'UP';
+            break;
+        case 'a' :
+            directionState = 'LEFT';
+            break;
+        case 's' :
+            directionState = 'DOWN';
+            break;
+        case 'd' :
+            directionState = 'RIGHT';
+            break;
+    }
+})
 
